0ebc6ea9146139de851464a6ec6fedd62aa62d88
Srikalyan Megati <kalmegati@gmail.com>
2019-09-13 19:30:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length > 2
+    terminus = array.pop
+    terminus = "and " + terminus
+    array.push(terminus)
+    array.join(", ")
+  else
+    array.join(" and ")
+  end
 end
