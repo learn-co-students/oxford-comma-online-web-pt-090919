fa1cea2c38ce68327a890cc1cfae4a53b519044c
Daniel Matney <danieljmatney@gmail.com>
2019-09-17 15:54:04 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,26 @@
 def oxford_comma(array)
-
+  length = array.length
+  new_array = array
+  if length == 1
+    new_array = array.join
+  elsif length == 2
+    new_array = array.join(" and ")
+  elsif length == 3
+    counter = 0
+    until counter == length - 1
+      new_array[counter] << (", ")
+      counter += 1
+    end
+    new_array[-2] << ("and ")
+    new_array = new_array.join
+  else
+    counter = 0
+    until counter == length - 1
+      new_array[counter] << (", ")
+      counter += 1
+    end
+    new_array[-2] << ("and ")
+    new_array = new_array.join
+  end
+  new_array
 end
