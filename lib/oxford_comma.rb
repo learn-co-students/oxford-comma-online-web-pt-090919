43f07150d87f70f6c1076ee3cbb24e69d25be059
James Benjamin <james.abenj@gmail.com>
2019-09-18 03:35:44 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+new_string = ""
+  array.each do |element|
+   if array.size == 1 
+    new_string << "#{element}"
+ elsif 
+ array.size == 2 && element == array[0]
+ new_string << "#{element} "
+  elsif
+  array.size > 2 && element != array[-1]
+    new_string << "#{element}, "
+  else
+    new_string << "and #{element}"
+  end
+end
+  return new_string
 end
